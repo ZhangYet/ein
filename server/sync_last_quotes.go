cf34fded63b5c3f65a270d0cb5b2e43ba197284a
zhangye <zhangyedante@didiglobal.com>
2019-02-16 12:07:46 +0800

    add sync_last_quotes

--- server/sync_last_quotes.go
+++ server/sync_last_quotes.go
@@ -1,1 +1,14 @@
 package server
+
+import "github.com/ZhangYet/ein/common"
+
+func (this EinServer) SyncLastQuotes() error {
+	quotes, err := common.GetLastQuotes()
+	if err != nil {
+		return err
+	}
+	for _, quote := range quotes {
+		common.QuoteData[quote.Symbol] = quote
+	}
+	return nil
+}
