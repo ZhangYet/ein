d058db0f699d4e63a6c7540c098fa9b1fec896a1
Dante <zhangyet@gmail.com>
2019-02-16 15:49:35 +0800

    update test

--- server/get_last_quote_test.go
+++ server/get_last_quote_test.go
@@ -3,15 +3,30 @@ package server
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/ZhangYet/ein"
+	"github.com/ZhangYet/ein/common"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func init() {
+	common.QuoteData = map[string]*ein.LastQuote{
+		"IBM": &ein.LastQuote{
+			Symbol: "IBM",
+			Time:   time.Now().Unix(),
+			Price:  42.0,
+			Size:   100,
+		},
+	}
+}
+
 func TestGetLastQuote(t *testing.T) {
 	ctx := context.Background()
-	req := &ein.QuoteRequest{}
+	req := &ein.QuoteRequest{
+		Key: "IBM",
+	}
 
 	res, err := cli.GetLastQuote(ctx, req)
 	assert.Nil(t, err)
