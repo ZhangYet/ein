4787028a5046d48bbdfd2c9ef05a8a7970d647c7
Dante <zhangyet@gmail.com>
2019-02-17 21:11:49 +0800

    使用 grpc_logrus 记录日志

--- ein/main.go
+++ ein/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
-	_ "net/http/pprof"
+	"os"
 	"time"
+
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
 
 	"github.com/lileio/fromenv"
 	"github.com/lileio/lile"
 	"github.com/lileio/logr"
 	"github.com/lileio/pubsub"
 	"github.com/lileio/pubsub/middleware/defaults"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 
 	"github.com/ZhangYet/ein"
@@ -17,12 +20,15 @@ import (
 	"github.com/ZhangYet/ein/ein/cmd"
 	"github.com/ZhangYet/ein/server"
 
+	_ "net/http/pprof"
+
 	opentracing "github.com/opentracing/opentracing-go"
 	zipkintracer "github.com/openzipkin/zipkin-go-opentracing"
 )
 
+var logrusLogger = logrus.New()
+
 func main() {
-	logr.SetLevelFromEnv()
 	recorder := zipkintracer.NewInMemoryRecorder()
 	tracer, err := zipkintracer.NewTracer(recorder)
 	if err != nil {
@@ -36,6 +42,14 @@ func main() {
 	lile.Server(func(g *grpc.Server) {
 		ein.RegisterEinServer(g, s)
 	})
+	logrusLogger.SetFormatter(&logrus.JSONFormatter{})
+	logFile, err := os.Create("ein.log")
+	if err != nil {
+		panic(err)
+	}
+	logrusLogger.SetOutput(logFile)
+	logrusEntry := logrus.NewEntry(logrusLogger)
+	lile.AddUnaryInterceptor(grpc_logrus.UnaryServerInterceptor(logrusEntry))
 
 	ticker := time.NewTicker(time.Second * 3)
 	defer ticker.Stop()
@@ -45,9 +59,10 @@ func main() {
 		for range ticker.C {
 			err := s.SyncLastQuotes()
 			if err != nil {
-				logr.WithCtx(ctx).Error(err)
+				logrusLogger.Error(err)
 			} else {
 				logr.WithCtx(ctx).Info(common.UpdateQuotaInfo)
+				logrusLogger.Info(common.UpdateQuotaInfo)
 			}
 
 		}
